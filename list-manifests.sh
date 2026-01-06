9a643b889d296e0c35d5f81c5de9cf1e6a50311d
eepeychev <eepeychev@yettel.bg>
2026-01-06 14:19:19 +0200

    add ArgoCD apps

--- list-manifests.sh
+++ list-manifests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # cat all manifest files under Tekton-Pipelines
 
-mapfile -t list < <(find Tekton-Pipelines -name "*.yaml" -o -name "*.yml" -o -name "*.json")
+mapfile -t list < <(find ArgoCD-demo-apps -name "*.yaml" -o -name "*.yml" -o -name "*.json")
 
 for file in "${list[@]}"; do
   cat "$file"
